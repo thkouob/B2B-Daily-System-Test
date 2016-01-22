7b98e4f26ea1080bb88b002ec8e854280dccc8a7
SeanChen <livebreeze@gmail.com>
2016-01-22 17:06:01 +0800

    modify create project page to B2BHome controll, and update back log block

--- B2BDR2.UI/Domain/Data.cs
+++ B2BDR2.UI/Domain/Data.cs
@@ -12,8 +12,8 @@ namespace B2BDR2.UI.Domain
         {
             var menu = new List<Navbar>();
             menu.Add(new Navbar { Id = 19, nameOption = "Mockup", imageClass = "fa fa-magic fa-fw", status = true, isParent = true, parentId = 0 });
-            menu.Add(new Navbar { Id = 20, nameOption = "Project Status", controller = "Home", action = "ProjectStatus", status = true, isParent = false, parentId = 19 });
-            menu.Add(new Navbar { Id = 21, nameOption = "Create Project", controller = "Home", action = "CreateProject", status = true, isParent = false, parentId = 19 });
+            menu.Add(new Navbar { Id = 20, nameOption = "Project Status", controller = "B2BHome", action = "ProjectStatus", status = true, isParent = false, parentId = 19 });
+            menu.Add(new Navbar { Id = 21, nameOption = "Create Project", controller = "B2BHome", action = "CreateProject", status = true, isParent = false, parentId = 19 });
 
             menu.Add(new Navbar { Id = 1, nameOption = "Dashboard", controller = "Home", action = "Index", imageClass = "fa fa-dashboard fa-fw", status = true, isParent = false, parentId = 0 });
             menu.Add(new Navbar { Id = 2, nameOption = "Charts", imageClass = "fa fa-bar-chart-o fa-fw", status = true, isParent = true, parentId = 0 });
