d775380ad934da2eb05aa99687a21281508df5fc
Leo.L.Li <leo.l.li@newegg.com>
2016-01-22 11:25:17 +0800

    Create Project

---
+++ B2BDR2.UI/Controllers/NavbarController.cs
@@ -0,0 +1,20 @@
+﻿using B2BDR2.UI.Domain;
+using B2BDR2.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace B2BDR2.UI.Controllers
+{
+    public class NavbarController : Controller
+    {
+        // GET: Navbar
+        public ActionResult Index()
+        {
+            var data = new Data();
+            return PartialView("_Navbar", data.navbarItems().ToList());
+        }
+    }
+}
---
+++ B2BDR2.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="B2BDR2.UI.MvcApplication" Language="C#" %>
