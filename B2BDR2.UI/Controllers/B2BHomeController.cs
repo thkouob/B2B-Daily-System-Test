4c3b8f46ebef69841c20e190771dbfbed59bfeab
SeanChen <livebreeze@gmail.com>
2016-01-26 10:28:26 +0800

    modify create sub task & replace variable

--- B2BDR2.UI/Controllers/B2BHomeController.cs
+++ B2BDR2.UI/Controllers/B2BHomeController.cs
@@ -22,12 +22,5 @@ namespace B2BDR2.UI.Controllers
         {
             return View();
         }
-
-        #region Modal
-        public ActionResult CreateSubTask()
-        {
-            return View();
-        }
-        #endregion
     }
 }
