65a1cf27764cbc36b7470cf960e166e40f3008ca
Sin.C.Lin <Sin.C.Lin@newegg.com>
2016-02-01 16:14:59 +0800

    test css

--- B2BDR2.UI/Scripts/Scrum/createProjectSin.ts
+++ B2BDR2.UI/Scripts/Scrum/createProjectSin.ts
@@ -2,16 +2,16 @@
     JiraNumber: string;
     JiraLink: string;
     Description: string;
-    SubTaskList: Array<SubTask2>;
+    SubTaskList: Array<SubTaskInfo>;
     constructor(jNumber: string, jLink: string, desc: string) {
         this.JiraNumber = jNumber;
         this.JiraLink = jLink;
         this.Description = desc;
-        this.SubTaskList = [new SubTask2("Dev-UI", []), new SubTask2("Dev-Service", []), new SubTask2("Test", [])];
+        this.SubTaskList = [new SubTaskInfo("Dev-UI", []), new SubTaskInfo("Dev-Service", []), new SubTaskInfo("Test", [])];
     }
 }
 
-class SubTask2 {
+class SubTaskInfo {
     Role: string;
     Assignee: Array<PersonInfo>;
     constructor(role: string, assignee: Array<PersonInfo>) {
@@ -39,7 +39,7 @@ class PersonInfo {
     }
 }
 
-angular.module('scrumModule', ['ngTagsInput', 'ui.bootstrap','ngAnimate'])
+angular.module('scrumModule', ['ngTagsInput', 'ui.bootstrap', 'ngAnimate'])
     .factory('DRService', ['$http', function ($http: ng.IHttpService) {
         //TODO Get API Data
         //$http.get('Url')
@@ -60,6 +60,7 @@ angular.module('scrumModule', ['ngTagsInput', 'ui.bootstrap','ngAnimate'])
     }])
     .factory('JIRAService', ['$http', function ($http: ng.IHttpService) {
         var getPersonUrl = 'http://10.16.133.102:52332/prj/v1/Person';
+        var getBackLogUrl = 'http://10.16.133.102:3000/jiraapi/issues';
 
         function GetPersonData(url) {
             var personInfoList: Array<PersonInfo> = [];
@@ -71,13 +72,32 @@ angular.module('scrumModule', ['ngTagsInput', 'ui.bootstrap','ngAnimate'])
                             new PersonInfo(value.UID, value.Name, value.Title, value.FirstName, value.GroupID, value.MemberType, value.MemberTypeDesc));
                     });
                 }, function (error) {
+                    //// write log / show error alert.
                     console.log(error);
                 });
             return personInfoList;
         }
 
+        function GetBackLogList(url) {
+            var backLogList: Array<BacklogInfo2> = [];
+            $http.get(url)
+                .then(function (response) {
+                    var result: any = response.data;
+                    angular.forEach(result.data, function (value, key) {
+                        backLogList.push(new BacklogInfo2(value.key, "http://jira/browse/" + value.key, value.summary));
+                    });
+                    $('#iconLoading').hide();
+                    $('#t_BackLog').fadeIn();
+                }, function (error) {
+                    //// write log / show error alert.
+                    console.log(error);
+                });
+            return backLogList;
+        }
+
         return {
-            GetPersonData: GetPersonData(getPersonUrl)
+            GetPersonData: GetPersonData(getPersonUrl),
+            GetBackLogList: GetBackLogList(getBackLogUrl)
         }
     }])
     .filter('personData', function () {
@@ -97,7 +117,7 @@ angular.module('scrumModule', ['ngTagsInput', 'ui.bootstrap','ngAnimate'])
 
             $scope.projectNumber;
             $scope.projectName;
-            $scope.BackLogList = DRService.BackLogList;
+            $scope.BackLogList = JIRAService.GetBackLogList;
             $scope.PersonData = JIRAService.GetPersonData;
             $scope.LoadPersonData = function (query) {
                 return $filter('personData')($scope.PersonData, query);
@@ -106,21 +126,20 @@ angular.module('scrumModule', ['ngTagsInput', 'ui.bootstrap','ngAnimate'])
             $scope.AddedProjectPBInfo = [];
             var projectPBInfoList: Array<BacklogInfo2> = [];
             //Test data for added project PB infos
-            projectPBInfoList.push(new BacklogInfo2("TCBB-9397", "http://jira/browse/TCBB-9397", "facase to api phase I (Landing, Product)"));
-            projectPBInfoList.push(new BacklogInfo2("TCBB-9220", "http://jira/browse/TCBB-9220", "Support MS, MW, Stand alone sent in SciQuest app"));
+            //projectPBInfoList.push(new BacklogInfo2("TCBB-9397", "http://jira/browse/TCBB-9397", "facase to api phase I (Landing, Product)"));
+            //projectPBInfoList.push(new BacklogInfo2("TCBB-9220", "http://jira/browse/TCBB-9220", "Support MS, MW, Stand alone sent in SciQuest app"));
 
             $scope.AddedProjectPBInfo = projectPBInfoList;
 
             $scope.AddPB = function ($index) {
                 var selectedPB: BacklogInfo2 = $scope.BackLogList[$index];
-                selectedPB.SubTaskList = [new SubTask2("Dev-UI", []), new SubTask2("Dev-Service", []), new SubTask2("Test", [])];
-                $scope.BackLogList.splice($index, 1);
+                selectedPB.SubTaskList = [new SubTaskInfo("Dev-UI", []), new SubTaskInfo("Dev-Service", []), new SubTaskInfo("Test", [])];
                 $scope.AddedProjectPBInfo.push(selectedPB);
+                $scope.BackLogList.splice($index, 1);
             }
 
             $scope.RemovePB = function ($index) {
-                var removedPB: BacklogInfo2 = $scope.AddedProjectPBInfo[$index];
+                $scope.BackLogList.push($scope.AddedProjectPBInfo[$index]);
                 $scope.AddedProjectPBInfo.splice($index, 1);
-                $scope.BackLogList.push(removedPB);
             }
         }]);
