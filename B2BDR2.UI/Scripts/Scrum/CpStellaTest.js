6eb3de6592d9d608486fc5c1d43b57b3839dc031
Stella.W.Chen <Stella.W.Chen@newegg.com>
2016-02-02 17:26:49 +0800

    add post model

--- B2BDR2.UI/Scripts/Scrum/CpStellaTest.js
+++ B2BDR2.UI/Scripts/Scrum/CpStellaTest.js
@@ -10,19 +10,16 @@ var BackLog = (function () {
     return BackLog;
 })();
 var B2bMember = (function () {
-    function B2bMember(id, name, fname, title, mtype, gid) {
+    function B2bMember(id, name) {
         this.UID = id;
         this.Name = name;
-        this.FirstName = fname;
-        this.Title = title;
-        this.MerberType = mtype;
-        this.GroupId = gid;
     }
     return B2bMember;
 })();
 var ProjectB = (function () {
-    function ProjectB(id, title, active, sub) {
+    function ProjectB(id, key, title, active, sub) {
         this.Id = id;
+        this.Key = key;
         this.Title = title;
         this.Active = active;
         this.Subtask = sub;
@@ -87,12 +84,12 @@ tpscpPractice.controller("JiraCtrl", ['$scope', 'getBackLogList', 'getMemberList
             var tempSubTask = [];
             tempSubTask = GetBackLogWithSubTask($scope.BacklogList[idx].Key);
             if (tempData.length > 0) {
-                tempData.push(new ProjectB($scope.BacklogList[idx].Key, $scope.BacklogList[idx].Summary, "", tempSubTask));
+                tempData.push(new ProjectB($scope.BacklogList[idx].Id, $scope.BacklogList[idx].Key, $scope.BacklogList[idx].Summary, "", tempSubTask));
                 tempBackLog.push($scope.BacklogList[idx]);
                 $scope.BacklogList.splice(idx, 1);
             }
             else {
-                tempData.push(new ProjectB($scope.BacklogList[idx].Key, $scope.BacklogList[idx].Summary, "in active", tempSubTask));
+                tempData.push(new ProjectB($scope.BacklogList[idx].Id, $scope.BacklogList[idx].Key, $scope.BacklogList[idx].Summary, "in active", tempSubTask));
                 tempBackLog.push($scope.BacklogList[idx]);
                 $scope.BacklogList.splice(idx, 1);
             }
@@ -103,12 +100,22 @@ tpscpPractice.controller("JiraCtrl", ['$scope', 'getBackLogList', 'getMemberList
             subTaskInfoList.push(new SubTask(key, "UI", null), new SubTask(key, "Service", null), new SubTask(key, "Test", null));
             return subTaskInfoList;
         }
+        function RoleToNumber(role) {
+            switch (role) {
+                case "Test":
+                    return 1;
+                case "Service":
+                    return 2;
+                case "UI":
+                    return 3;
+            }
+        }
         ////add a subTask to pb ---------------------------------------------------------------////
         $scope.AddSubTask = function (pbId, role, asign, idx) {
             var keepGoing = true;
             angular.forEach($scope.ProjectList, function (value, key) {
                 if (keepGoing) {
-                    if (value.Id === pbId) {
+                    if (value.Key === pbId) {
                         value.Subtask.push(new SubTask(pbId, role, asign));
                     }
                 }
@@ -119,13 +126,12 @@ tpscpPractice.controller("JiraCtrl", ['$scope', 'getBackLogList', 'getMemberList
             var keepGoing = true;
             angular.forEach(tempBackLog, function (value, key) {
                 if (keepGoing) {
-                    if (value.Key === $scope.ProjectList[idx].Id) {
+                    if (value.Key === $scope.ProjectList[idx].Key) {
                         $scope.BacklogList.push(new BackLog(value.Id, value.Key, value.Summary, value.Description, value.Assignee, value.Reporter));
                         keepGoing = false;
                     }
                 }
             });
-            debugger;
             tempData.splice(idx, 1);
             //fix if remove first tag, content area will be disappear
             if (tempData[0].Active === "") {
@@ -135,7 +141,7 @@ tpscpPractice.controller("JiraCtrl", ['$scope', 'getBackLogList', 'getMemberList
         //while click <li> update class active, then the tag will work correct when remove it. 
         $scope.UpdateActiveClass = function (idx) {
             angular.forEach($scope.ProjectList, function (value, key) {
-                if (value.Key === $scope.ProjectList[idx].Id) {
+                if (value.Key === $scope.ProjectList[idx].Key) {
                     value.Active = "in active";
                 }
                 else {
@@ -148,25 +154,49 @@ tpscpPractice.controller("JiraCtrl", ['$scope', 'getBackLogList', 'getMemberList
             var keepGoing = true;
             angular.forEach($scope.ProjectList, function (value, key) {
                 if (keepGoing) {
-                    if (value.Id === pbId) {
+                    if (value.Key === pbId) {
                         value.Subtask.splice(idx, 1);
                     }
                 }
             });
         };
         ////Assignee tags display ---------------------------------------------------------------////
-        $scope.loadAssigneeTag = function ($query) {
-            var countries = $scope.MembersList;
-            return countries.filter(function (country) {
-                return country.Name.toLowerCase().indexOf($query.toLowerCase()) != -1
-                    || country.UID.toLowerCase().indexOf($query.toLowerCase()) != -1;
+        $scope.LoadAssigneeTag = function ($query) {
+            var b2bMembers = [];
+            angular.forEach($scope.MembersList, function (value, key) {
+                b2bMembers.push(new B2bMember(value.UID, value.Name));
+            });
+            return b2bMembers.filter(function (member) {
+                return member.Name.toLowerCase().indexOf($query.toLowerCase()) != -1
+                    || member.UID.toLowerCase().indexOf($query.toLowerCase()) != -1;
             });
         };
-        $scope.tagAdded = function (tag) {
-            $scope.log.push('Added: ' + tag.text);
+        $scope.AllFormData = {
+            PBList: GetPbList()
         };
-        $scope.tagRemoved = function (tag) {
-            $scope.log.push('Removed: ' + tag.text);
-        };
+        //$scope.$watchCollection('ProjectList', function (newNames, oldNames) {
+        //debugger
+        //});
+        function GetPbList() {
+            var pbInfo = [];
+            angular.forEach($scope.ProjectList, function (value, key) {
+                var slist = [];
+                angular.forEach(value.Subtask, function (sval, key) {
+                    angular.forEach(sval.Assign, function (val, key) {
+                        slist.push({
+                            AssigneeUID: val.UID,
+                            Role: sval.Role
+                        });
+                    });
+                });
+                pbInfo.push({
+                    IssueId: value.Id,
+                    IssueKey: value.Key,
+                    Selected: true,
+                    SubTaskList: slist
+                });
+            });
+            return pbInfo;
+        }
     }]);
 //# sourceMappingURL=CpStellaTest.js.map
