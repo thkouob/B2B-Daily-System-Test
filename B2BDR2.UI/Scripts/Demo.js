8ff2f0982b87a75efc62ed5179ad02bead25498e
Leo.L.Li <leo.l.li@newegg.com>
2016-02-18 15:18:06 +0800

    fix project status style

--- B2BDR2.UI/Scripts/Demo.js
+++ B2BDR2.UI/Scripts/Demo.js
@@ -195,9 +195,9 @@ angular.module('mvcapp', [])
     .factory('projectService', ['$http', function ($http) {
         var mockData = [];
         mockData.push(new ProjectStatus(12815, "B2B_WWW Newkit phase I", "Sean.Z.Chen", "2015/12/19", JIRAStatus.process, DRStatus.done));
-        mockData.push(new ProjectStatus(12778, "B2B_WWW 2016 CW 2-4:BOM Phase III & Prodcut Fix", "Jac.T.Wang", "2015/12/18", JIRAStatus.done, DRStatus.done));
+        mockData.push(new ProjectStatus(12778, "B2B_WWW 2016 CW 2-4:BOM Phase III & Prodcut Fix", "Jac.T.Wang", "2015/12/18", JIRAStatus.done, DRStatus.error));
         mockData.push(new ProjectStatus(12777, "B2B_WWW Newkit phase II", "Sean.Z.Chen", "2015/12/17", JIRAStatus.done, DRStatus.process));
-        mockData.push(new ProjectStatus(12776, "B2B_WWW 2016 CW 2-3:BOM Phase II", "Jac.T.Wang", "2015/12/16", JIRAStatus.done, DRStatus.done));
+        mockData.push(new ProjectStatus(12776, "B2B_WWW 2016 CW 2-3:BOM Phase II", "Jac.T.Wang", "2015/12/16", JIRAStatus.done, DRStatus.none));
         return {
             DataList: mockData
         };
@@ -206,11 +206,23 @@ angular.module('mvcapp', [])
         $scope.DataList = projectService.DataList;
         $scope.GetTdClass = function (value) {
             //https://docs.angularjs.org/api/ng/service/$sce
-            return value == DRStatus.process || value == JIRAStatus.process ? $sce.trustAsHtml('<p class="fa fa-spinner" /><span>x</span>') : $sce.trustAsHtml('<p class="fa fa-check" /><span>v</span>');
+            var statusCss;
+            switch (value) {
+                case JIRAStatus.done:
+                    statusCss = '<p class="fa fa-check" title="Done" />';
+                    break;
+                case JIRAStatus.error:
+                    statusCss = '<p class="fa fa-times" title="Error" />';
+                    break;
+                case JIRAStatus.process:
+                    statusCss = '<p class="fa fa-spinner" title="Processing..." />';
+                    break;
+            }
+            return $sce.trustAsHtml(statusCss);
         };
         $scope.SearchVal;
         $scope.RemoveSearchVal = function () {
-            $scope.SearchVal = null;
+            $scope.SearchVal = "";
         };
         JiraService.GetBacklog.then(function (data) {
             console.info(data);
