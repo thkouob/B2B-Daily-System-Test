8730411cf499fc8e17426d7e4eee841c5bf7f616
Stella.W.Chen <Stella.W.Chen@newegg.com>
2016-02-24 17:20:37 +0800

    init

---
+++ B2BDR2.UI/Controllers/B2BHomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace B2BDR2.UI.Controllers
+{
+    public class B2BHomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }        
+
+        public ActionResult CreateProject()
+        {
+            return View("CreateProject");
+        }
+
+        public ActionResult ProjectStatus()
+        {
+            return View();
+        }
+
+        #region Modal
+        public ActionResult CreateSubTask()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
---
+++ B2BDR2.UI/Controllers/NavbarController.cs
@@ -0,0 +1,26 @@
+﻿using B2BDR2.UI.Domain;
+using B2BDR2.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace B2BDR2.UI.Controllers
+{
+    public class NavbarController : Controller
+    {
+        // GET: Navbar
+        public ActionResult Index()
+        {
+            var data = new Data();
+            return PartialView("_Navbar", data.navbarItems().ToList());
+        }
+
+        public ActionResult B2BIndex()
+        {
+            var data = new Data();
+            return PartialView("_B2BNavbar", data.GetB2BNavbarItems().ToList());
+        }
+    }
+}
