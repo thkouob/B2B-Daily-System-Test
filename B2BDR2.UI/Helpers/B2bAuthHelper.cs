f65dda7410f6a636fcf25084edf3bef096f14d27
Stella.W.Chen <Stella.W.Chen@newegg.com>
2016-02-25 09:52:52 +0800

    add helper

---
+++ B2BDR2.UI/Helpers/B2bAuthHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace B2BDR2.UI
+{
+    public class B2bAuthHelper
+    {
+        private static bool IsExist
+        {
+            get
+            {
+                return HttpContext.Current != null;
+            }
+        }
+
+        private static IPrincipal _user
+        {
+            get
+            {
+                if (IsExist)
+                {
+                    return HttpContext.Current.User;
+                }
+                return null;
+            }
+        }
+
+        /// <summary>ad short name</summary>
+        public static string CurrentUserName
+        {
+            get
+            {
+                if (_user != null && _user.Identity.IsAuthenticated)
+                {
+                    var userName = _user.Identity.Name;
+                    return userName.Substring(userName.LastIndexOf(@"\") + 1);
+                }
+                return null;
+            }
+        }
+    }
+}
